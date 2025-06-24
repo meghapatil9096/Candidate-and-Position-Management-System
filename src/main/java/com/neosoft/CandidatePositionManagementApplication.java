0a533da32ff34ba7f20f03191c4ec209557018fc
meghapatil9096 <meghapatil9096@gmail.com>
2025-06-24 11:36:48 +0530

    send

--- src/main/java/com/neosoft/CandidatePositionManagementApplication.java
+++ src/main/java/com/neosoft/CandidatePositionManagementApplication.java
@@ -1,10 +1,9 @@
 package com.neosoft;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class CandidatePositionManagementApplication {
@@ -12,10 +11,5 @@ public class CandidatePositionManagementApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CandidatePositionManagementApplication.class, args);
 	}
-	@Bean
-	public ObjectMapper objectMapper() {
-		ObjectMapper mapper = new ObjectMapper();
-		mapper.registerModule(new JavaTimeModule()); // ✅ enables LocalDate parsing
-		return mapper;
-	}
+
 }
